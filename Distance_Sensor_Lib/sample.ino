fb38ab86fc1e83f980b2963f36554427250e0e6c
1sand0s(Aditya T) <1sand0s@users.noreply.github.com>
2015-08-24 15:01:57 +0530

    Update sample.ino

--- Distance_Sensor_Lib/sample.ino
+++ Distance_Sensor_Lib/sample.ino
@@ -1,15 +1,15 @@
 #include <ultra_sense.h>
 
-Trans t;
+Trans t; // Object to set the ultrasonic sensor parameters 
 void setup()
 {
-  t.Trig=12;
-  t.Echo=11;
+  t.Trig=12; //Set Trig pin as digital pin 12, can be varied
+  t.Echo=11; //Set Echo pin as digital pin 11, can be varied
   Serial.begin(9600);
 }
 void loop()
 {
-  t.begin();
-  t.print("centi");
+  t.begin();// Start the sensing 
+  t.print("centi"); // Replace centi with inch for calculation in inch
   delay(100);
 }
